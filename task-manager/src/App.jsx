e92e68e9026838977a8b5d6f4a2bd9e85a155dec
Reinhardt-Analytics <168109247+Reinhardt-Analytics@users.noreply.github.com>
2025-10-30 15:41:14 -0500

    Bugfix on TaskList

---
+++ task-manager/src/App.jsx
@@ -0,0 +1,3 @@
+import { TaskList } from './TaskList';
+
+console.log('Hello')
--- task-manager/src/components/TaskList.jsx
+++ task-manager/src/components/TaskList.jsx
@@ -1,7 +1,22 @@
-const taskevents = [
+import React from 'react';
+
+const tasks = [
     { id: 1, title: 'Find a book.', description: 'Check availability within the library and select one for rental.' },
     { id: 2, title: 'Read the book.', description: 'Read the contents of the book and enjoy at your own leasure.' },
     { id: 3, title: 'Return the boon.', description: 'After reading the book, be sure to return it to the library.' }
-]
+];
 
-export default taskevents;
+function TaskList() {
+    return (
+        tasks.map(task => (
+            <div key={task.id} className='task-item'>
+                <Link link='/task/${task.id}'>
+                    <h3>{task.title}</h3>
+                </Link>
+                <p>{task.description}</p>
+            </div>
+        ))
+    );
+}
+
+export default TaskList;
