0f17c97f1f3be4cce5c2c305c960c36eb5b69ee4
Reinhardt-Analytics <168109247+Reinhardt-Analytics@users.noreply.github.com>
2025-10-29 21:24:16 -0500

    Simple initialization.

---
+++ task-manager/src/components/TaskList.jsx
@@ -0,0 +1,7 @@
+const taskevents = [
+    { id: 1, title: 'Find a book.', description: 'Check availability within the library and select one for rental.' },
+    { id: 2, title: 'Read the book.', description: 'Read the contents of the book and enjoy at your own leasure.' },
+    { id: 3, title: 'Return the boon.', description: 'After reading the book, be sure to return it to the library.' }
+]
+
+export default taskevents;
